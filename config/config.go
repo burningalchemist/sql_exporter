fa6a03d2a91036d4fca4c0896d470d4f76cd625e
Hugo Hromic <hhromic@gmail.com>
2023-01-24 23:53:27 +0000

    fix: initialize global defaults if not provided in config

--- config/config.go
+++ config/config.go
@@ -40,7 +40,7 @@ func Load(configFile string) (*Config, error) {
 
 // Config is a collection of jobs and collectors.
 type Config struct {
-	Globals        *GlobalConfig      `yaml:"global"`
+	Globals        *GlobalConfig      `yaml:"global,omitempty"`
 	CollectorFiles []string           `yaml:"collector_files,omitempty"`
 	Target         *TargetConfig      `yaml:"target,omitempty"`
 	Jobs           []*JobConfig       `yaml:"jobs,omitempty"`
@@ -57,6 +57,14 @@ func (c *Config) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	type plain Config
 	if err := unmarshal((*plain)(c)); err != nil {
 		return err
+	}
+
+	if c.Globals == nil {
+		c.Globals = &GlobalConfig{}
+		// Force a dummy unmarshall to populate global defaults
+		if err := c.Globals.UnmarshalYAML(func(interface{}) error { return nil }); err != nil {
+			return err
+		}
 	}
 
 	if (len(c.Jobs) == 0) == (c.Target == nil) {
