02072f9d33e09398f3227a2aa3807bff201c8f0b
Sergei Zyubin <git@szyubin.me>
2023-09-19 11:49:03 +0200

    refactor: 'instance' label is configurable, 'target' by default

--- target.go
+++ target.go
@@ -50,15 +50,15 @@ type target struct {
 	conn *sql.DB
 }
 
-// NewTarget returns a new Target with the given instance name, data source name, collectors and constant labels.
+// NewTarget returns a new Target with the given target name, data source name, collectors and constant labels.
 // An empty target name means the exporter is running in single target mode: no synthetic metrics will be exported.
 func NewTarget(
-	logContext, name, dsn string, ccs []*config.CollectorConfig, constLabels prometheus.Labels, gc *config.GlobalConfig) (
+	logContext, tname, dsn string, ccs []*config.CollectorConfig, constLabels prometheus.Labels, gc *config.GlobalConfig) (
 	Target, errors.WithContext,
 ) {
-	if name != "" {
-		logContext = fmt.Sprintf("%s, target=%q", logContext, name)
-		constLabels = prometheus.Labels{"instance": name}
+	if tname != "" {
+		logContext = fmt.Sprintf("%s, target=%q", logContext, tname)
+		constLabels = prometheus.Labels{config.TargetLabel: tname}
 	}
 
 	constLabelPairs := make([]*dto.LabelPair, 0, len(constLabels))
@@ -82,7 +82,7 @@ func NewTarget(
 	upDesc := NewAutomaticMetricDesc(logContext, upMetricName, upMetricHelp, prometheus.GaugeValue, constLabelPairs)
 	scrapeDurationDesc := NewAutomaticMetricDesc(logContext, scrapeDurationName, scrapeDurationHelp, prometheus.GaugeValue, constLabelPairs)
 	t := target{
-		name:               name,
+		name:               tname,
 		dsn:                dsn,
 		collectors:         collectors,
 		constLabels:        constLabels,
