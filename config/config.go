643a65ea57cc6bf6d1528cd4a7812c57f5405363
Sergei Zyubin <git@szyubin.me>
2023-09-25 21:48:00 +0200

    fix: minor refactoring

--- config/config.go
+++ config/config.go
@@ -22,13 +22,12 @@ import (
 var (
 	EnablePing  bool
 	DsnOverride string
+	TargetLabel string
 )
 
 // MaxInt32 defines the maximum value of allowed integers
 // and serves to help us avoid overflow/wraparound issues.
 const MaxInt32 int = 1<<31 - 1
-
-var TargetLabel string
 
 // Load attempts to parse the given config file and return a Config object.
 func Load(configFile string) (*Config, error) {
