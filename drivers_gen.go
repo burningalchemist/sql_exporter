ec18c84ef00cd79d6c31761e38fb7900c184a857
Sergei Zyubin <sergei.zyubin@gmail.com>
2026-09-01 12:13:58 +0000

    revert: update go-ora driver to v3 (#1074)

--- drivers_gen.go
+++ drivers_gen.go
@@ -23,7 +23,7 @@ var driverList = map[string][]string{
 	},
 	"extra": {
 		"github.com/ClickHouse/clickhouse-go/v2",
-		"github.com/sijms/go-ora/v3",
+		"github.com/sijms/go-ora/v2",
 		"github.com/jackc/pgx/v5/stdlib",
 	},
 	"commercial": {
